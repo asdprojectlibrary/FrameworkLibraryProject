044f374d5f28d6d474eb3ae049d5bc55117feaa7
ecadet-mum <51096249+ecadet-mum@users.noreply.github.com>
2019-08-08 08:13:04 -0400

    ok

--- LibraryFramework/src/main/java/Repository/BaseRepository.java
+++ LibraryFramework/src/main/java/Repository/BaseRepository.java
@@ -5,12 +5,14 @@ import config.IConfig;
 import config.LibraryManager;
 import dao.IDataAccess;
 import dao.flatfile.FlatFileDataAccess;
+import dao.rdb.ModelDataAccess.ConcreteDataAccessFactory;
 import dao.rdb.ModelDataAccess.DataAccessFactory;
 import model.BaseModel;
 
 final public class BaseRepository<T extends BaseModel> {
 
     IDataAccess<T> dataAccess;
+    DataAccessFactory dataAccessFactory= ConcreteDataAccessFactory.getInstance();
 
     public BaseRepository(Class<T> type) {
 
@@ -21,7 +23,7 @@ final public class BaseRepository<T extends BaseModel> {
                 this.dataAccess = new FlatFileDataAccess<>(type);
             } else {
                 //Factory method
-                this.dataAccess = DataAccessFactory.getInstance().getDataAccess(type);
+                this.dataAccess = dataAccessFactory.getDataAccess(type);
             }
         }
 
